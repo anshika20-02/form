44777f7eb7128775391593f552ffd2acad88ee3d
anshika20-02 <anshikapatel201@gmail.com>
2023-04-13 16:08:51 +0530

    form

---
+++ post.php
@@ -0,0 +1,13 @@
+<?php
+               echo"welcome<br/>";
+                 $a=$_POST["name"];
+                 $b=$_POST["phone"];
+                 $c=$_POST["pswd"];
+                               echo "hii $a welcome <br>";
+		echo "Your Registered mobile No. is $b<br>";
+		echo "Your Password is $c<br>";
+
+          
+      echo"you are successfully registered<br>";
+  echo"for new Registration click<a href='form.html'>here</a><br>";
+?>
